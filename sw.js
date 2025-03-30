44a1959a5f7dc0caa3b58a7a24b2bdfcc93e594f
titenustitle <titenustitle1024@gmail.com>
2025-03-30 15:41:45 +0600

    sw.js uploaded

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'memory-saver-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/css/styles.css',
+    '/js/db.js',
+    '/js/fileManager.js',
+    '/js/ui.js',
+    '/js/app.js',
+    '/icons/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then((response) => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+}); 
